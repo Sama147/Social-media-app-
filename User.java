5f92f2290a48087f64e079a0ed915b75aed2060b
Sama Ashraf <60572513+Sama147@users.noreply.github.com>
2024-05-09 19:59:10 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,108 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class User
+{
+    private String username;
+    private String bio;
+    private String profilePicture;
+    private final List<User> friendList;
+    private ArrayList<Post> posts;
+    private ArrayList<Comment> comments;
+    private ArrayList<Post> likes;
+
+    public User()
+    {
+        this.friendList = new ArrayList<>();
+    }
+
+    public User(String username, String bio, String profilePicture)
+    {
+        this.username = username;
+        this.bio = bio;
+        this.profilePicture = profilePicture;
+        this.friendList = new ArrayList<>();
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setBio(String bio)
+    {
+        this.bio = bio;
+    }
+
+    public void setProfilePicture(String profilePicture)
+    {
+        this.profilePicture = profilePicture;
+    }
+
+
+    public String getProfilePicture()
+    {
+        return profilePicture;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+    public Object getPassword()
+    {
+        Object Password;
+        return null;
+    }
+
+    public String getBio()
+    {
+        return bio;
+    }
+
+
+    public ArrayList<Post> getPosts()
+    {
+        return posts;
+    }
+
+    public void setPosts(ArrayList<Post> posts)
+    {
+        this.posts = posts;
+
+    }
+
+    public ArrayList<Comment> getComments()
+    {
+        return comments;
+    }
+
+    public void setComments(ArrayList<Comment> comments)
+    {
+        this.comments = comments;
+    }
+
+    public  ArrayList<Post> getLikes()
+    {
+        return Likes;
+    }
+    public void setLikes (ArrayList<Post> likes)
+    {
+        this.likes = likes;
+    }
+
+    public List<User> getFriendList() {
+        return friendList;
+    }
+
+    public void addFriend(User friend) {
+        friendList.add(friend);
+    }
+
+    public void removeFriend(User friend) {
+        friendList.remove(friend);
+    }
+
+}
